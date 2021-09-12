f3b0ff133fc72e68796810be603b687b1e2bfd1c
Sarthak Khandekar <sarthak@Sarthaks-MacBook-Pro.local>
2021-09-12 23:29:13 +0530

    Docker file addition

--- AuthBackend/asgi.py
+++ AuthBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for auth_backend_service project.
+ASGI config for AuthBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auth_backend_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AuthBackend.settings")
 
 application = get_asgi_application()
